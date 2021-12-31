dbb9bd5a7877aad3cb27a9ad79710597a8817ace
ArerehRaj <raj.jr@somaiya.edu>
2021-12-31 16:28:29 +0530

    Added code for horizontal list view for showing the most searched taxes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,82 @@ import '../helpers/common_code.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  Widget getTaxCard(
+    double devicewidth,
+    double deviceHeight,
+    String country,
+    String taxName,
+    Color bgColor,
+  ) {
+    return Container(
+      width: devicewidth * 0.4,
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 5,
+        ),
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+              child: Container(
+                color: bgColor,
+                width: double.infinity,
+                child: Image.asset(
+                  'assets/images/text.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            getSizedBox(
+              deviceHeight * 0.005,
+            ),
+            FittedBox(
+              child: Text(
+                taxName,
+                style: const TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            getSizedBox(
+              deviceHeight * 0.005,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 5,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FittedBox(
+                    child: Text(
+                      country,
+                      style: const TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  const Icon(
+                    Icons.book_outlined,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   Widget getSizedBox(double height) {
     return SizedBox(
       height: height,
@@ -34,6 +110,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final deviceSize = CommonCode.getDeviceSize(context);
+
+    final mostSearchTaxesList = [
+      {
+        'tax_name': 'Income Tax',
+        'country': 'India',
+        'color': Colors.lightGreen.shade300,
+      },
+      {
+        'tax_name': 'GST',
+        'country': 'India',
+        'color': Colors.red.shade100,
+      },
+      {
+        'tax_name': 'Wealth Tax',
+        'country': 'Japan',
+        'color': Colors.lightBlue.shade300,
+      },
+      {
+        'tax_name': 'Services Tax',
+        'country': 'UAE',
+        'color': Colors.pink.shade100,
+      },
+    ];
 
     final searchWidget = Card(
       margin: const EdgeInsets.symmetric(
@@ -75,53 +174,86 @@ class HomeScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
       ),
-      body: Container(
-        height: deviceSize.height,
-        width: deviceSize.width,
-        child: Column(
-          children: [
-            searchWidget,
-            getSizedBox(
-              deviceSize.height * 0.025,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                getTaxTypeIcons(
-                  'Income Tax',
-                  Colors.green.shade300,
+      body: SingleChildScrollView(
+        child: Container(
+          height: deviceSize.height,
+          width: deviceSize.width,
+          child: Column(
+            children: [
+              searchWidget,
+              getSizedBox(
+                deviceSize.height * 0.025,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  getTaxTypeIcons(
+                    'Income Tax',
+                    Colors.green.shade300,
+                  ),
+                  getTaxTypeIcons(
+                    'GST',
+                    Colors.red.shade200,
+                  ),
+                  getTaxTypeIcons(
+                    'Services',
+                    Colors.blue.shade200,
+                  ),
+                ],
+              ),
+              getSizedBox(
+                deviceSize.height * 0.025,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  getTaxTypeIcons(
+                    'Wealth Tax',
+                    Colors.purple.shade400,
+                  ),
+                  getTaxTypeIcons(
+                    'Corporate Tax',
+                    Colors.lightGreen.shade200,
+                  ),
+                  getTaxTypeIcons(
+                    'VAT',
+                    Colors.yellow.shade200,
+                  ),
+                ],
+              ),
+              getSizedBox(
+                deviceSize.height * 0.025,
+              ),
+              const Text(
+                'Mostly Search',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
                 ),
-                getTaxTypeIcons(
-                  'GST',
-                  Colors.red.shade200,
+                textAlign: TextAlign.left,
+              ),
+              getSizedBox(
+                deviceSize.height * 0.025,
+              ),
+              SizedBox(
+                height: deviceSize.height * 0.3,
+                width: deviceSize.width,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: mostSearchTaxesList.length,
+                  itemBuilder: (ctx, index) {
+                    return getTaxCard(
+                      deviceSize.width,
+                      deviceSize.height,
+                      mostSearchTaxesList[index]['country'].toString(),
+                      mostSearchTaxesList[index]['tax_name'].toString(),
+                      mostSearchTaxesList[index]['color'] as Color,
+                    );
+                  },
                 ),
-                getTaxTypeIcons(
-                  'Services',
-                  Colors.blue.shade200,
-                ),
-              ],
-            ),
-            getSizedBox(
-              deviceSize.height * 0.025,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                getTaxTypeIcons(
-                  'Wealth Tax',
-                  Colors.purple.shade400,
-                ),
-                getTaxTypeIcons(
-                  'Corporate Tax',
-                  Colors.lightGreen.shade200,
-                ),
-                getTaxTypeIcons(
-                  'VAT',
-                  Colors.yellow.shade200,
-                ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
