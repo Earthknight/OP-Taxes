5c45d97551b2cb2d34fe1eedbf5cde7e83ee453d
Lalu Nair <62281187+Earthknight@users.noreply.github.com>
2022-01-04 21:24:43 +0530

    Long definition tax question and answer widget

---
+++ lib/widgets/fl_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:taxes/modal/que_ans.dart';
+
+import 'mytext.dart';
+
+class LongTerm_W {
+  Widget LongQuestion(){
+    return Container(
+      alignment: Alignment.topLeft,
+      padding: const EdgeInsets.only(left: 10,right:10,top: 15),
+      margin: const EdgeInsets.only(left: 10,right: 10),
+      child: MyText(QuesAns().questions, 17, const Color(0xff036D21), '', FontWeight.bold),
+    );
+  }
+  Widget LongAnswer(){
+    return Container(
+      alignment: Alignment.topLeft,
+      padding: const EdgeInsets.only(left: 10,right:10,top: 15),
+      margin: const EdgeInsets.only(left: 10,right: 10),
+      child: MyText(QuesAns().answers, 16,Colors.black, '', FontWeight.w600)
+    );
+  }
+}
+
+
+
